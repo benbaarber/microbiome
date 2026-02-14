7a8dbe738a59f2cc67eb987252d851abd09264c0
Ben Barber <benbarber121@gmail.com>
2026-02-14 18:23:28 -0500

    reset config

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,7 @@ use microbiome::simulation::WorldConfig;
 #[macroquad::main("Microbiome")]
 async fn main() {
     let cli = Cli::parse();
-    let config = WorldConfig {
-        cell_count: 20,
-        cell_mass: 5000.0,
-        cell_explosion_threshold: Some(10000.0),
-        ..Default::default()
-    };
+    let config = WorldConfig::default();
     let mut game = Game::start(&cli, config);
     loop {
         if game.update() {
